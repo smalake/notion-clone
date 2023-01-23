13e0499e18f7ef2b63eb1bd19311a6b808c6b2b3
smalake <koizumim1002@gmail.com>
2023-01-23 21:05:48 +0900

    メモを追加する機能の実装

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -50,6 +50,17 @@ const Sidebar = () => {
     setActiveIndex(activeIndex);
   }, [navigate]);
 
+  const addMemo = async () => {
+    try {
+      const res = await memoApi.create();
+      const newMemos = [res, ...memos];
+      dispatch(setMemo(newMemos));
+      navigate(`memo/${res._id}`);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
     <Drawer
       container={window.document.body}
@@ -110,7 +121,7 @@ const Sidebar = () => {
             <Typography variant="body2" fontWeight="700">
               プライベート
             </Typography>
-            <IconButton>
+            <IconButton onClick={() => addMemo()}>
               <AddCircleOutlineIcon fontSize="small" />
             </IconButton>
           </Box>
